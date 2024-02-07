381580d7d0f7af4c0d8dd240d088fa9fdca160f9
Mark Shevchenko <markshevchenkopu@Marks-MacBook-Pro-2.local>
2024-02-07 15:38:26 +0100

    Cleaning up staff, and refining the code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import perf_counter
 
 from scraper.scrapy_test import deploy_crawler
+import pandas as pd
 
 
 if __name__ == "__main__":
@@ -15,7 +16,14 @@ if __name__ == "__main__":
     t = perf_counter()
 
     deploy_crawler()
+
+    #to rename columns if needed
     
+    df = pd.read_csv('output.csv')
+    df2 = df.rename(columns = {'id':"Property ID"}, inplace = False)
+    df2.to_csv("output.csv")
+
+
 
     
     print("\nFinal time taken to scrape specs on all the properties:", perf_counter() - t)
--- scraper/scrapy_test.py
+++ scraper/scrapy_test.py
@@ -6,7 +6,7 @@ class ImmoSpider(scrapy.Spider):
     name = "immoweb"
     allowed_domains = ["immoweb.be"]
     start_urls = []
-    for x in range(1):
+    for x in range(333):
         start_urls.append(f"https://www.immoweb.be/en/search/house/for-sale?countries=BE&page={x}&orderBy=postal_code")
         # start_urls.append(f"https://www.immoweb.be/en/search/appartement/for-sale?countries=BE&page={x}&orderBy=postal_code")
     
@@ -20,7 +20,7 @@ class ImmoSpider(scrapy.Spider):
         price = response.css("p.classified__price span.sr-only::text").get()
         price = price.replace("€","")
         house_subtypes = [
-    "cungalow",
+    "bungalow",
     "chalet",
     "castle",
     "farmhouse",
@@ -48,10 +48,23 @@ class ImmoSpider(scrapy.Spider):
             "property type": property_type,
             "property subtype": split_url[0],
             "type of sale": split_url[1],
+            "Bedrooms": None,
+            "Living area" : None,
+            "Kitchen type": 1,
+            "Furnished": 0,
+            "Open fire": 0,
+            "Terrace surface": None,
+            "Garden surface": None,#sometimes in description only
+            "Surface of the plot": None,
+            "Number of facades": None, #from description
+            "Swimming pool": 0,
+            "Building condition": None
+
         }
-        for row in response.css("tbody.classified-table__body"):
+        for row in response.css("tr.classified-table__row"):
             key = row.css("th.classified-table__header::text").get()
             value = row.css("td.classified-table__data::text").get()
+
 
             if not (key and value): continue
             
@@ -67,27 +80,39 @@ class ImmoSpider(scrapy.Spider):
                 value_mapping = {
                     "Yes": 1,
                     "No": 0,
-                    "Not specified": None
+                    "Not specified": None,
+
                 }
 
                 # Convert value using the dictionary mapping
                 value = value_mapping.get(value, value)
+            if key == "Kitchen type":
+                value = 0 if value == "Not installed" else 1
 
-            data[key] = value
+            if key in data.keys():
+                data[key] = value
+
+
+            print(f"Key: {key}, Value: {value}")
 
         yield data
-
+        
 def deploy_crawler():
     # Specify the output file and format
     output_settings = {
-        "FEED_FORMAT": "csv",
-        "FEED_URI": "output.csv",
         "FEEDS": {
             "output.csv": {
                 "format": "csv",
                 "overwrite": True,
-            }
+                "fields_to_export": None,  # Export all fields
+                "export_empty_fields": True,  # Include null values for missing fields
+            },
+            "output.json": {
+                "format": "json",
+                "overwrite": True,
+            },
         },
+        "DOWNLOAD_DELAY": 2,  # Set the delay to 2 seconds between requests
     }
 
     # Create a CrawlerProcess with the spider and output settings
@@ -96,6 +121,4 @@ def deploy_crawler():
 
     # Start the crawling process
     process.start()
-
-deploy_crawler()
 
