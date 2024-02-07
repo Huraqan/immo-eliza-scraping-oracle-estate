b5a4e0d314f40c644e83eb8c2e713b87ca0a70dd
Mark Shevchenko <markshevchenkopu@Marks-MacBook-Pro-2.local>
2024-02-07 09:22:04 +0100

    cosmetic changes

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import json
-from random import uniform
-from time import perf_counter, sleep
-
-from pandas import DataFrame
-from requests import Session
-
+from time import perf_counter
 
 from scraper.scrapy_test import deploy_crawler
 
@@ -19,23 +13,10 @@ if __name__ == "__main__":
     )
 
     t = perf_counter()
-    # for x in range(333):
-    #     start_urls.append(f"https://www.immoweb.be/en/search/house/for-sale?countries=BE&page={x}&orderBy=postal_code")
-    #     # start_urls.append(f"https://www.immoweb.be/en/search/appartement/for-sale?countries=BE&page={x}&orderBy=postal_code")
-
-    # deploy_crawler()
-
-    # for x in range(333):
-    #     start_urls.append(f"https://www.immoweb.be/en/search/appartement/for-sale?countries=BE&page={x}&orderBy=postal_code")
 
     deploy_crawler()
     
-    with open("output.json", "r") as file:
-        content = json.load(file)
-        
-        data = DataFrame.from_dict(content)
-        
-        # print(data)
+
     
     print("\nFinal time taken to scrape specs on all the properties:", perf_counter() - t)
         
