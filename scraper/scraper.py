f9a5d1310b50455a833d925230eeff5f0a9aa57f
unknown <indesteege.sebastiaan@gmail.com>
2024-02-07 09:40:12 +0100

    updated scrapy code

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -14,8 +14,21 @@ class ImmoWebScraper:
         self.base_url: str = "www.immoweb.be/"
         self.data_dict: dict = {}
         self.property_urls: set = set()
+        self.property_urls_l: list = list()
         self.session: Session = session
 
+    def try_except_decorator(function):
+        def decorated(*args, **kwargs):
+            while True:
+                try:
+                    return function(*args, **kwargs)
+                except:
+                    if input("\nCONNECTION ERROR! Try again? 'n' to quit : ") == "n":
+                        exit()
+
+        return decorated
+
+    @try_except_decorator
     def make_simple_request(self, url: str):
         print("url:", url)
         return self.session.get(url)
@@ -33,15 +46,24 @@ class ImmoWebScraper:
         )
 
         links_set = set()
+        links_list = list()
+        
         for property_url in property_urls:
             if "new-real-estate-project-apartments" in property_url:
                 continue
-
+            
+            links_list.append(property_url["href"])
             links_set.add(property_url["href"])
 
-        print("Total links scraped: ", len(links_set))
+        print(f"Total links scraped: set: {len(links_set)}, list: {len(links_list)}")
 
         self.property_urls.update(links_set)
+        self.property_urls_l += links_list
+        
+        from collections import Counter
+        print("\nList duplicates:", [k for k,v in Counter(links_list).items() if v>1])
+        # print("list:\n", sorted(links_list))
+        # print("set:\n", sorted(links_set))
 
     def property_urls_to_txt(self):
         with open("urls.txt", "w") as file:
@@ -60,14 +82,17 @@ class ImmoWebScraper:
         for dictionary in parsed_json:
             set_of_field_names.update(dictionary.keys())
 
-        field_names = list(set_of_field_names)
+        sorted_field_names = sorted(list(set_of_field_names))
 
-        writer = DictWriter(csv_file, fieldnames=field_names)
+        writer = DictWriter(csv_file, fieldnames=sorted_field_names)
 
         writer.writeheader()
-
-        # for country, leaders in self.leaders_data.items():
-        #     for leader in leaders:
-        #         writer.writerow({"Country": country, **leader})
+        
+        # write rows
+        for dictionary in parsed_json:
+            row = {}
+            for field_name in sorted_field_names:
+                row[field_name] = dictionary.get(field_name, None)
+            writer.writerow(row)
 
         print("Done saving CSV file.\n")
